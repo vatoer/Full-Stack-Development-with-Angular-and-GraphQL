2ca2ef92edcc89e97abc3eabeca2f5804687984e
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-04-15 11:10:05 +0200

    Exposing a simple GraphQL API

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,41 @@
-import  express, { Application } from "express";
-const PORT = 8080;
-const app: Application = express();
+import express, { Application } from "express";
+import { ApolloServer, ApolloServerOptions } from "@apollo/server";
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import { json } from 'body-parser';
+import { gql } from '@apollo/client/core';
+import { IResolvers } from "@graphql-tools/utils";
 
-app.get('/', (req, res) => res.send('Express is successfully running!!'));
-app.listen(PORT, () => {
-    console.log(`'Server is running at http://localhost:${PORT}`)
-});
+const typeDefs = gql`
+    type Query {
+        message: String!
+        }
+    `;
+
+const resolvers: IResolvers = {
+    Query: {
+        message: () => 'It works!'
+    }
+};
+
+const config = {
+    typeDefs: typeDefs,
+    resolvers: resolvers
+}
+
+async function startApolloServer(config: any) {
+    const PORT = 8080;
+    const app: Application = express();
+
+    const server: ApolloServer = new ApolloServer(config);
+
+    await server.start()
+    server.assertStarted('expressMiddleware()');
+    app.use('/graphql', cors<cors.CorsRequest>(), json(), expressMiddleware(server));
+
+    app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`);
+    })
+}
+
+startApolloServer(config);
